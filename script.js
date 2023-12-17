be188075f92217f8ec242fccc559ac3568b601ee
Marshal ram <marshaljr615@gmailcom>
2023-12-17 23:49:05 +0545

    redirect_page

---
+++ script.js
@@ -0,0 +1,7 @@
+// let links = document.querySelectorAll(".direct");
+function navigateToPage(page){
+    window.location.href=page;
+}
+// links.forEach(link=>{
+//     link.addEventListener("click",myFunc)
+// });
